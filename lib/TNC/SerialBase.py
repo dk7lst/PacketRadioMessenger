f2fcc7415f1193eb1fa2fa91b41c7035f3782829
Lars Struss <mail@lsnet.eu>
2023-11-11 16:40:29 +0100

    Serial port configuration improved and logging of port settings added.

--- lib/TNC/SerialBase.py
+++ lib/TNC/SerialBase.py
@@ -9,7 +9,13 @@ class SerialBase:
       baudrate=config.getInt(section, "devicebaud", 9600),
       bytesize=config.getInt(section, "bytesize", serial.EIGHTBITS),
       parity=config.getString(section, "parity", serial.PARITY_NONE),
+      stopbits=config.getInt(section, "stopbits", serial.STOPBITS_ONE),
+      xonxoff=config.getBool(section, "xonxoff", False),
+      rtscts=config.getBool(section, "rtscts", False),
+      dsrdtr=config.getBool(section, "dsrdtr", False),
+      exclusive=config.getBool(section, "exclusive", True),
       timeout=None)
+    logging.info('Serial port setting for port "%s": %s' % (self.ser.port, str(self.ser.get_settings())))
 
   def __del__(self):
     self.ser.close()
